a1120409eacdb2b70e2aeef12bfef30d29eb78e1
Julien LEICHER <j.leicher@biolog-id.com>
2019-06-03 14:20:54 +0200

    chore: Update spenx to 1.1.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
   install_requires=[
     'click~=7.0',
     'Arpeggio~=1.9.0',
-    'spenx~=1.0.2',
+    'spenx~=1.1.0',
     'Jinja2~=2.10.1',
     'markdown~=3.0.1',
     'beautifulsoup4~=4.6.3',
